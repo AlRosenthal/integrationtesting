2a5064dede7203526079703d343c2839be58b76d
Al Rosenthal <arosenthal@itgroove.ca>
2018-06-26 15:19:04 -0700

    added multiplcation

--- src/Calc.ts
+++ src/Calc.ts
@@ -5,5 +5,8 @@ export class Calc {
   public subtract(num1: number, num2: number) : number {
     return num1 - num2;
   }
+  public multiply(num1: number, num2: number) : number {
+    return num1 * num2;
+  }
 }
 export default Calc;
--- tests/Calc.test.ts
+++ tests/Calc.test.ts
@@ -13,4 +13,9 @@ describe('Calc test suite', function() {
     let result = calc.subtract(20,10);
     result.should.equals(10, `Should return: 10, but returned: ${result}`);
   });
+  
+  it ('Can multiply', function () {
+    let result = calc.multiply(2, 10);
+    result.should.equals(20, `Should return: 20, but returned: ${result}`);
+  });
 });
